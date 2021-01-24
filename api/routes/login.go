17f589eb5d6db450b23e3e87caf2d2b7c33b9320
Sourik Ghosh <sourikghosh31@gmail.com>
2021-01-24 15:18:56 +0530

    models attached

--- api/routes/login.go
+++ api/routes/login.go
@@ -1,9 +1,18 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"restapi/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 //Login function
 func Login(c *gin.Context) {
+	var loginData models.Login
+	if err := c.ShouldBind(&loginData); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
 	c.JSON(200, gin.H{
 		"success": "Login😏",
 	})
