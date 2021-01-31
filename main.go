5ef903fdfdbf806fc0343a7b4d29938292ea3360
Sourik Ghosh <sourikghosh31@gmail.com>
2021-01-31 20:37:20 +0530

    updated

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
-	"restapi/api/routes"
+	"log"
+	"restapi/api/handler"
+	"restapi/api/middleware"
 	"restapi/config"
+	"restapi/database"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	err := database.InitDB()
+	if err != nil {
+		log.Fatalf("⛔ Unable to connect to database: %v\n", err)
+	} else {
+		log.Println("DATABASE CONNECTED 🥇")
+	}
 	app := gin.Default()
 	api := app.Group("/api")
 	{
-		api.POST("/login", routes.Login)
-		api.POST("/signup", routes.Signup)
+		api.POST("/login", middleware.LoginAuth(), handler.Login)
+		api.POST("/signup", middleware.SignupAuth(), handler.Signup)
 	}
 
 	app.Run(":" + config.Config["PORT"])
