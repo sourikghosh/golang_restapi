81abef9407b38f46e7a02ed6a6d2e8af7e3d5df8
Sourik Ghosh <sourikghosh31@gmail.com>
2021-01-31 20:37:13 +0530

    modified

--- Database/table.sql
+++
@@ -1,6 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TABLE go_userlist (
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v1(),
-    email VARCHAR(150) UNIQUE NOT NULL,
-    password VARCHAR(150) NOT NULL
-);
