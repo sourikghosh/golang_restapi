8f0b7695e9b56952c32d92449a514d8ebb4449f5
Sourik Ghosh <sourikghosh31@gmail.com>
2021-02-02 19:26:44 +0530

    modified

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+
 	"restapi/api/handler"
 	"restapi/api/middleware"
 	"restapi/config"
@@ -10,18 +11,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	err := database.InitDB()
-	if err != nil {
-		log.Fatalf("⛔ Unable to connect to database: %v\n", err)
+func init() {
+	errRedis := database.RedisClient(config.Config["REDIS_URL"])
+	if errRedis != nil {
+		log.Fatalf("⛔ Redis URI is not valid: %v\n", errRedis)
+	} else {
+		log.Println("REDIS CONNECTED 🥇")
+	}
+
+	errDB := database.InitDB(config.Config["DATABASE_URL"])
+	if errDB != nil {
+		log.Fatalf("⛔ Unable to connect to database: %v\n", errDB)
 	} else {
 		log.Println("DATABASE CONNECTED 🥇")
 	}
+}
+
+func main() {
+
 	app := gin.Default()
 	api := app.Group("/api")
 	{
-		api.POST("/login", middleware.LoginAuth(), handler.Login)
-		api.POST("/signup", middleware.SignupAuth(), handler.Signup)
+		api.POST("/login", middleware.LoginAuthentication(), handler.Login)
+		api.POST("/signup", middleware.SignupAuthentication(), handler.Signup)
+		protected := api.Group("/protected")
+		{
+			protected.GET("/some-route", handler.RouteHandler)
+		}
 	}
 
 	app.Run(":" + config.Config["PORT"])
