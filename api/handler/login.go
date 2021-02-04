26f545db3fb7a563ccf9056245937120d9d2f297
Sourik Ghosh <sourikghosh31@gmail.com>
2021-02-04 15:30:01 +0530

    updated

--- api/handler/login.go
+++ api/handler/login.go
@@ -1,13 +1,33 @@
 package handler
 
 import (
+	"restapi/api/controller"
+	"restapi/models"
+
 	"github.com/gin-gonic/gin"
 )
 
 //Login function
-func Login(c *gin.Context) {
+func Login(ctx *gin.Context) {
+	data := ctx.MustGet("data").(models.Signup)
+	tokenDetails, err := controller.CreateToken(data.ID)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	errToken := controller.SetToken(ctx, data.ID, tokenDetails)
+	if errToken != nil {
+		ctx.JSON(500, gin.H{
+			"error": errToken.Error(),
+		})
+		return
+	}
 
-	c.JSON(200, gin.H{
-		"success": "Logged In😏",
+	ctx.JSON(200, gin.H{
+		"access_token":  tokenDetails.AccessToken,
+		"refresh_token": tokenDetails.RefreshToken,
 	})
+
 }
