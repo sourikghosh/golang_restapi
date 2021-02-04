66d26be2266c4ce4cc02c7f1aadcad8da5362745
Sourik Ghosh <sourikghosh31@gmail.com>
2021-02-04 19:09:14 +0530

    updated

--- api/handler/someRoute.go
+++ api/handler/someRoute.go
@@ -1,25 +1,14 @@
 package handler
 
 import (
-	"restapi/api/controller"
-
 	"github.com/gin-gonic/gin"
 )
 
 //RouteHandler for handleing routes of some-route
 func RouteHandler(ctx *gin.Context) {
-	tokenAuth, err := controller.ExtractTokenMetadata(ctx)
-	if err != nil {
-		ctx.AbortWithStatusJSON(401, gin.H{"error": "Unauthorized"})
-		return
-	}
-	userID, err := controller.FetchSetTokens(ctx, tokenAuth)
-	if err != nil {
-		ctx.AbortWithStatusJSON(401, gin.H{"error": "Unauthorized"})
-		return
-	}
+	userid := ctx.MustGet("userid").(string)
 
 	ctx.JSON(200, gin.H{
-		"success": userID,
+		"success": userid,
 	})
 }
