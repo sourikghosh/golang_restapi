ff1ee711b351d0071b1b7b1e90d8074c9fa75202
Sourik Ghosh <sourikghosh31@gmail.com>
2021-02-04 19:09:07 +0530

    updated

--- api/handler/signup.go
+++ api/handler/signup.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"restapi/api/controller"
 	"restapi/database"
 	"restapi/models"
 
@@ -10,7 +11,30 @@ import (
 //Signup routes
 func Signup(ctx *gin.Context) {
 	data := ctx.MustGet("data").(models.Signup)
-
 	database.CreateUser(ctx, data.Email, data.Password)
-	ctx.JSON(201, gin.H{"success": "Created 😋"})
+
+	tokenDetails, err := controller.CreateToken(data.ID)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	errToken := controller.SetToken(ctx, data.ID, tokenDetails)
+	if errToken != nil {
+		ctx.JSON(500, gin.H{
+			"error": errToken.Error(),
+		})
+		return
+	}
+	ctx.SetCookie("jid", tokenDetails.RefreshToken,
+		604800,
+		"/api/ref",
+		"localhost",
+		false,
+		true,
+	)
+	ctx.JSON(201, gin.H{
+		"access_token": tokenDetails.AccessToken,
+	})
 }
